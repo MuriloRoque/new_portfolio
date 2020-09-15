6ba06ec19ec289e7bc6cdf7215949840e2647506
Murilo Roque <muriloengqui@gmail.com>
2020-09-15 11:59:11 -0300

    structure ProjectsList making a file for my projects list

--- src/containers/ProjectsList.js
+++ src/containers/ProjectsList.js
@@ -1,1 +1,25 @@
 import projectsList from '../styles/ProjectsList.module.css';
+import React from 'react';
+import PROJECTS from '../constants/projects';
+import Project from '../components/Project';
+
+const ProjectsList = () => (
+  <section>
+    <h1>My Portfolio</h1>
+    {
+      PROJECTS.map(project => (
+        <Project
+          key={project.id}
+          title={project.title} 
+          stacks={project.stacks} 
+          description={project.description} 
+          liveLink={project.liveLink}
+          repo={project.repo}
+          images={project.images}
+        />
+      ))
+    }
+  </section>
+);
+
+export default ProjectsList;
