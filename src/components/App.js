9522e425c777f01567d0c6edfad6964983b6d6f2
Murilo Roque <muriloengqui@gmail.com>
2020-09-15 10:46:46 -0300

    structure Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
 const App = () => (
-  <div />
+  <div>
+    <Header />
+  </div>
 );
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,19 @@
 import header from '../styles/Header.module.css';
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <div>
+      MR
+    </div>
+    <nav>
+      <ul>
+        <li>Portfolio</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
