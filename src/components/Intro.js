08dd2063671bc7541e4c86eaf219bd0ac22e0f7c
Murilo Roque <muriloengqui@gmail.com>
2020-09-17 12:30:05 -0300

    style header on 1024px size

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -13,42 +13,44 @@ import instagram from '../assets/images/instagram.png';
 const Intro = () => (
   <section className={intro.container}>
     <Header />
-    <div className={intro.intro}>
-      <h1>Murilo Roque</h1>
-      <h4>Full-stack Developer</h4>
-      <p>
-        Hello, I’m Murilo, a Full-stack Developer and Chemical Engineer
-        who specializes in Ruby, Rails, Javascript, Node, Express and React.
-        Currently, I am exploring remote opportunities.
-      </p>
-    </div>
-    <div className={intro.network}>
-      <span>My works in social networks</span>
-      <div className={intro.iconContainer}>
-        <a rel="noopener noreferrer" target='_blank'  href="https://github.com/MuriloRoque">
-          <img src={github} alt="github" />
-        </a>
-        <a rel="noopener noreferrer" target='_blank'  href="https://twitter.com/MuriloRoquePai1">
-          <img src={twitter} alt="twitter" />
-        </a>
-        <a rel="noopener noreferrer" target='_blank'  href="https://www.linkedin.com/in/murilo-roque/">
-          <img src={linkedin} alt="linkedin" />
-        </a>
-        <a rel="noopener noreferrer" target='_blank'  href="https://angel.co/u/murilo-roque-paiva-da-silva">
-          <img src={angellist} alt="angellist" />
-        </a>
-        <a rel="noopener noreferrer" target='_blank'  href="https://medium.com/@muriloengqui">
-          <img src={medium} alt="medium" />
-        </a>
-        <a rel="noopener noreferrer" target='_blank'  href="https://stackexchange.com/users/17775586/murilo-roque-paiva-da-silva">
-          <img src={stack} alt="stack" />
-        </a>
-        <a rel="noopener noreferrer" target='_blank'  href="https://www.facebook.com/murilo.roque.52">
-          <img src={facebook} alt="facebook" />
-        </a>
-        <a rel="noopener noreferrer" target='_blank'  href="https://www.instagram.com/muriloroquep/?hl=en">
-          <img src={instagram} alt="instagram" />
-        </a>
+    <div className={intro.intro_network}>
+      <div className={intro.intro}>
+        <h1>Murilo Roque</h1>
+        <h4>Full-stack Developer</h4>
+        <p>
+          Hello, I’m Murilo, a Full-stack Developer and Chemical Engineer
+          who specializes in Ruby, Rails, Javascript, Node, Express and React.
+          Currently, I am exploring remote opportunities.
+        </p>
+      </div>
+      <div className={intro.network}>
+        <span>My works in social networks</span>
+        <div className={intro.iconContainer}>
+          <a rel="noopener noreferrer" target='_blank'  href="https://github.com/MuriloRoque">
+            <img src={github} alt="github" />
+          </a>
+          <a rel="noopener noreferrer" target='_blank'  href="https://twitter.com/MuriloRoquePai1">
+            <img src={twitter} alt="twitter" />
+          </a>
+          <a rel="noopener noreferrer" target='_blank'  href="https://www.linkedin.com/in/murilo-roque/">
+            <img src={linkedin} alt="linkedin" />
+          </a>
+          <a rel="noopener noreferrer" target='_blank'  href="https://angel.co/u/murilo-roque-paiva-da-silva">
+            <img src={angellist} alt="angellist" />
+          </a>
+          <a rel="noopener noreferrer" target='_blank'  href="https://medium.com/@muriloengqui">
+            <img src={medium} alt="medium" />
+          </a>
+          <a rel="noopener noreferrer" target='_blank'  href="https://stackexchange.com/users/17775586/murilo-roque-paiva-da-silva">
+            <img src={stack} alt="stack" />
+          </a>
+          <a rel="noopener noreferrer" target='_blank'  href="https://www.facebook.com/murilo.roque.52">
+            <img src={facebook} alt="facebook" />
+          </a>
+          <a rel="noopener noreferrer" target='_blank'  href="https://www.instagram.com/muriloroquep/?hl=en">
+            <img src={instagram} alt="instagram" />
+          </a>
+        </div>
       </div>
     </div>
   </section>
